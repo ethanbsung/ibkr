49ee326ae9772002a4009c4c7aa2c27522710130
ethanbsung <ebsung@umich.edu>
2024-12-09 17:24:51 -0500

    meanrev

--- meanrev_live.py
+++ meanrev_live.py
@@ -1,24 +1,32 @@
 '''
+bollinger_period = 15
+bollinger_stddev = 2
+stop_loss_points = 3
+take_profit_points = 23
+commission_per_side = 0.47
+total_commission = commission_per_side * 2
+initial_cash = 5000
+
 Performance Summary:
 Start Date               :      2023-12-18
 End Date                 :      2024-12-08
-Exposure Time            :          35.27%
-Final Account Balance    :       $9,791.66
-Equity Peak              :       $9,883.58
-Total Return             :          95.83%
-Annualized Return        :          60.92%
+Exposure Time            :          31.41%
+Final Account Balance    :       $9,669.40
+Equity Peak              :       $9,823.48
+Total Return             :          93.39%
+Annualized Return        :          59.50%
 Benchmark Return         :          23.89%
-Volatility (Annual)      :          12.27%
-Total Trades             :             461
-Winning Trades           :             134
-Losing Trades            :             327
-Win Rate                 :          29.07%
-Profit Factor            :            1.56
-Sharpe Ratio             :            3.06
-Sortino Ratio            :           34.66
-Calmar Ratio             : 17.751457314383575
-Max Drawdown             :          -5.40%
-Average Drawdown         :          -1.42%
-Max Drawdown Duration    :       1.81 days
-Average Drawdown Duration:       0.10 days
+Volatility (Annual)      :          10.77%
+Total Trades             :             490
+Winning Trades           :              96
+Losing Trades            :             394
+Win Rate                 :          19.59%
+Profit Factor            :            1.74
+Sharpe Ratio             :            3.20
+Sortino Ratio            :           59.35
+Calmar Ratio             : 18.073190509060993
+Max Drawdown             :          -5.17%
+Average Drawdown         :          -1.46%
+Max Drawdown Duration    :       2.75 days
+Average Drawdown Duration:       0.15 days
 '''
